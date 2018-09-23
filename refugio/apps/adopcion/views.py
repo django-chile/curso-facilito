6bea507a79f3403cce6fd720b80ab350cd68a932
marlon <falconsoft.3d@gmail.com>
2018-09-23 15:03:07 -0300

    6- Relaciones

--- refugio/apps/adopcion/views.py
+++ refugio/apps/adopcion/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index_adopcion(request):
+    return HttpResponse("Adopcion")
