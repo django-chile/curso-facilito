b10ae63eee205b945ad4fb1c5d28bc56656b3d57
marlon <falconsoft.3d@gmail.com>
2018-09-23 14:33:50 -0300

    6- Relaciones

--- refugio/apps/adopcion/admin.py
+++ refugio/apps/adopcion/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from apps.adopcion.models import Persona
 
 # Register your models here.
+
+admin.site.register(Persona)
--- refugio/apps/mascota/models.py
+++ refugio/apps/mascota/models.py
@@ -3,4 +3,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from apps.adopcion.models import Persona
+
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=50)
+
 # Create your models here.
+
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edad_aproximada = models.IntegerField()
+    fecha_rescate = models.DateField()
+
+    # Relacion Unos a Varios
+    persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+
+
+    # Relacion de Muchos es a Muchos
+    vacuna = models.ManyToManyField(Vacuna, blank=True)
