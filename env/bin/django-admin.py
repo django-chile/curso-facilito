1c356da210e89a41ed24e6bf07154ff890a16df5
marlon <falconsoft.3d@gmail.com>
2018-09-23 14:00:30 -0300

    4-Crear App y Estructura Proyecto

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/marlon/Documents/Programacion/djando/00/curso-facilito/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- refugio/refugio/settings.py
+++ refugio/refugio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.mascota',
+    'apps.adopcion',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'refugio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mascota',
+        'USER': 'odoo',
+        'PASSWORD': 'odoo',
+        'HOTS': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -103,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
